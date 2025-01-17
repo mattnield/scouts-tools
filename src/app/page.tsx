c0f7a1fbc543af520b7c1c51171e93a1b5efcdcf
Matt Nield <mattnield@gmail.com>
2025-01-17 00:18:58 +0000

    Make the header look nicer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,9 @@ import HeaderDropdown from '@/components/HeaderDropdown';
 export default async function HomePage() {
 
   return (
+
     <div>
-      <header style={{ padding: '16px', background: '#f4f4f4', borderBottom: '1px solid #ccc' }}>
-        <HeaderDropdown />
-      </header>
-      <main style={{ padding: '16px' }}>
-        <h1>Scouts Tools</h1>
-      </main>
+      <h2 className='text-xl font-semibold'>Scouts Tool</h2>
     </div>
   );
 }
